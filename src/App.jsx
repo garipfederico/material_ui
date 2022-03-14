b8e14bb8757fde5e4ce02fc973c3776520a64224
garipfederico <garip.federico@gmail.com>
2022-03-14 19:52:03 -0300

    Video1
    Titulo: Curso Material UI #01 Instalación [ React js con Material Design ]
    URL: https://www.youtube.com/watch?v=-ipl60m5eQA&list=PLPl81lqbj-4Kn-PRUvHuzh_591Euc3688
    Descripcion: Instalacion de material y prueba usando un boton.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@material-ui/core/Button'
+
+function App() {
+  return (
+    <div>
+      <Button>Este es un boton</Button>
+    </div>
+  );
+}
+
+export default App;
