a607f3678826190c24ea909a304877c66ec1e5a1
garipfederico <garip.federico@gmail.com>
2022-03-14 21:12:12 -0300

    Video2
    Curso Material UI #02 Botones [ React & Material Design ]
    URL: https://www.youtube.com/watch?v=gmsIJ2ALdyI&list=PLPl81lqbj-4Kn-PRUvHuzh_591Euc3688&index=2
    Descripcion: Configuracion de botones botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,74 @@
 import React from 'react'
-import Button from '@material-ui/core/Button'
+// import Button from '@material-ui/core/Button'
+//Son dos formas de importar distintas
+import { Button } from '@material-ui/core/'
+
+
+
 
 function App() {
   return (
     <div>
-      <Button>Este es un boton</Button>
+     
+     <Button color = "secondary">
+     color
+     </Button>
+     <Button 
+      color="primary"
+      variant="outlined"
+     >
+       outlined
+     </Button>
+     <p>Lorem</p>
+     
+     <Button 
+      color="primary"
+      variant="contained">
+       con sombra
+     </Button>
+    
+     <Button 
+      color="primary"
+      variant="contained"
+      disableElevation>
+       sin sombra
+     </Button>
+
+    <p>lorem</p>
+     <Button 
+      color="primary"
+      variant="contained"
+      href="https://google.com">
+       href a google
+     </Button>
+     <Button 
+      color="primary"
+      variant="contained"
+      fullWidth
+      size="large">
+       fullwidth
+     </Button>
+    
+     <Button 
+      color="primary"
+      variant="contained"
+      size="small">
+       small
+     </Button>
+     <Button 
+      color="primary"
+      variant="contained"
+      size="medium">
+       medium
+     </Button>
+     <Button 
+      color="primary"
+      variant="contained"
+      size="large">
+       large
+     </Button>
+    
+     
     </div>
   );
 }
